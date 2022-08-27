3d95ea4f65582c6d53891d761e92b2fcd06a3f4f
CankutER <cnkt.er@gmail.com>
2022-08-27 18:42:54 +0300

    implemented used letter blocking

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ function App() {
   const context = useGlobalContext();
   return (
     <main>
+      <div className="forbidden-letters">
+        <h3>Letters that are not in the secret word</h3>
+        <div className="letter-container">
+          {Array.from(context.trash).map((letter, i) => {
+            return (
+              <div className="forbidden-letter" key={i}>
+                {letter}
+              </div>
+            );
+          })}
+        </div>
+      </div>
       <header>
         <h1 className="title">Wordle on React</h1>
       </header>
--- src/context.js
+++ src/context.js
@@ -2,9 +2,7 @@ import React, { useContext, useEffect, useRef, useState } from "react";
 const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
-  console.log("rendered");
   const [solution, setSolution] = useState("cankut");
-
   const [guessNo, setGuessNo] = useState(0);
   const [guesses, setGuesses] = useState(
     solution.split("").map((letter, i) => {
@@ -16,26 +14,25 @@ export const AppProvider = ({ children }) => {
     })
   );
   const [typedNo, setTypedNo] = useState(0);
+  const [trash, setTrash] = useState(new Set());
 
   useEffect(() => {
-    // const initGuesses = solution.split("").map((letter, i) => {
-    //   return { id: i, text: new Array(solution.length).fill("") };
-    // });
-    // setGuesses(initGuesses);
     function write(e) {
       if (
         String(e.key).length === 1 &&
         String(e.key).match(/[a-z]/i) &&
         typedNo <= solution.length - 1
       ) {
-        setGuesses((guesses) => {
-          let temp = [...guesses];
+        if (!trash.has(e.key)) {
+          setGuesses((guesses) => {
+            let temp = [...guesses];
 
-          temp[guessNo].text[typedNo] = e.key;
-          return temp;
-        });
+            temp[guessNo].text[typedNo] = e.key;
+            return temp;
+          });
 
-        setTypedNo((typedNo) => typedNo + 1);
+          setTypedNo((typedNo) => typedNo + 1);
+        }
       }
       if (e.key === "Enter") {
         if (guesses[guessNo].text.join("").length !== solution.length) {
@@ -47,7 +44,16 @@ export const AppProvider = ({ children }) => {
           setGuessNo(guessNo + 1);
           setTypedNo(0);
         }
+        guesses[guessNo].text.forEach((letter, i) => {
+          if (!solution.includes(letter)) {
+            setTrash((trash) => {
+              trash.add(letter);
+              return trash;
+            });
+          }
+        });
       }
+
       if (e.key === "Backspace") {
         if (typedNo > 0) {
           setGuesses((guesses) => {
@@ -69,7 +75,7 @@ export const AppProvider = ({ children }) => {
   }, [typedNo, guessNo]);
   return (
     <AppContext.Provider
-      value={{ solution: solution.split(""), guesses: guesses }}
+      value={{ solution: solution.split(""), guesses: guesses, trash: trash }}
     >
       {children}
     </AppContext.Provider>
